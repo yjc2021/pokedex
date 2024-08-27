0ab28a732adf7ff196629eed61f6d7ac98de7db5
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2024-08-27 12:58:40 +0900

    Fix: Apply queryOptions, infiniteQueryOptions API to query factory

--- app/service/pokemon/queries.ts
+++ app/service/pokemon/queries.ts
@@ -1,19 +1,26 @@
+import { parseOffset } from "@/app/lib/utils";
+import { infiniteQueryOptions, queryOptions } from "@tanstack/react-query";
+
 import PokemonService from "./pokemon-service";
 
 const queryKeys = {
-  all: ["pokemon"] as const,
-  detail: (pokemonId: string) => [...queryKeys.all, pokemonId] as const,
+  all: () => ["pokemon"],
+  detail: (pokemonId: string) => [...queryKeys.all(), pokemonId] as const,
 };
 
-const queryOptions = {
-  all: () => ({
-    queryKey: queryKeys.all,
-    queryFn: () => PokemonService.getPokemon(),
-  }),
-  detail: (pokemonId: string) => ({
-    queryKey: queryKeys.detail(pokemonId),
-    queryFn: () => PokemonService.getPokemonDetail(pokemonId),
-  }),
+const queries = {
+  all: () =>
+    infiniteQueryOptions({
+      queryKey: queryKeys.all(),
+      queryFn: ({ pageParam }) => PokemonService.getPokemon({ offset: pageParam }),
+      getNextPageParam: (lastPage) => parseOffset(lastPage.next),
+      initialPageParam: 0,
+    }),
+  detail: (pokemonId: string) =>
+    queryOptions({
+      queryKey: queryKeys.detail(pokemonId),
+      queryFn: () => PokemonService.getPokemonDetail(pokemonId),
+    }),
 };
 
-export default queryOptions;
+export default queries;
